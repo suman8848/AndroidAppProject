5df4f41e0c5cb3119670446d874712eb139183ae
Suman <suman.khatiwada@hotmail.com>
2018-04-17 11:57:59 -0500

    development git test

--- app/src/main/java/com/example/sumankhatiwada/vehiclebazzar/base/BaseActivity.java
+++ app/src/main/java/com/example/sumankhatiwada/vehiclebazzar/base/BaseActivity.java
@@ -66,7 +66,7 @@ public abstract class BaseActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
 //        ButterKnife.(this);
-        super.onDestroy();
+        super.onDestroy(); 
     }
 
     protected void resolveDaggerDependency() {
