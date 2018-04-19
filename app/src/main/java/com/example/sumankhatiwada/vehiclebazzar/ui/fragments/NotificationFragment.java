43e8f1bd878888921eab91108a958cfcbb3fbf47
Suman <suman.khatiwada@hotmail.com>
2018-04-19 13:43:36 -0500

    dashboard design implementation with bottomnavigationview

---
+++ app/src/main/java/com/example/sumankhatiwada/vehiclebazzar/ui/fragments/NotificationFragment.java
@@ -0,0 +1,23 @@
+package com.example.sumankhatiwada.vehiclebazzar.ui.fragments;
+
+import android.content.Intent;
+import android.view.View;
+
+import com.example.sumankhatiwada.vehiclebazzar.R;
+import com.example.sumankhatiwada.vehiclebazzar.base.BaseFragment;
+
+/**
+ * Created by sumankhatiwada on 4/19/18.
+ */
+
+public class NotificationFragment extends BaseFragment {
+    @Override
+    public int getContentView() {
+        return R.layout.fragment_notification;
+    }
+
+    @Override
+    protected void onViewReadyFragment(View view, Intent intent) {
+        super.onViewReadyFragment(view, intent);
+    }
+}
